2907e335fd4ae0b9d8a5c51ea3dd6ec0a0662b85
unknown <faraimunashe.m11@gmail.com>
2023-06-14 10:20:29 +0200

    add:objective

--- resources/views/objective.blade.php
+++ resources/views/objective.blade.php
@@ -24,26 +24,24 @@
             </div>
         </div>
         <div class="col-12 list-group list-group-light">
-
-                <div class="row mt-3">
-                    <div class="col-12">
-                        <div class="card bg-secondary">
-                            <div class="">
-                                <span class="badge bg-primary">{{$objective->deadline}}</span>
-                                <button class="btn btn-danger btn-sm m-1" style="float: right;" data-bs-toggle="modal" data-bs-target="#deleteModal{{$objective->id}}">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                                <button class="btn btn-dark btn-sm m-1" style="float: right;" data-bs-toggle="modal" data-bs-target="#commentModal{{$objective->id}}">
-                                    <i class="bi bi-chat"></i>
-                                </button>
-                            </div>
-                            <div class="card-body text-center">
-                                <p class="pt-3 text-3xl text-white">{{$objective->objective}}</p>
-                            </div>
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card bg-secondary">
+                        <div class="">
+                            <span class="badge bg-primary">{{$objective->deadline}}</span>
+                            <button class="btn btn-danger btn-sm m-1" style="float: right;" data-bs-toggle="modal" data-bs-target="#deleteModal{{$objective->id}}">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                            <button class="btn btn-dark btn-sm m-1" style="float: right;" data-bs-toggle="modal" data-bs-target="#commentModal{{$objective->id}}">
+                                <i class="bi bi-chat"></i>
+                            </button>
+                        </div>
+                        <div class="card-body text-center">
+                            <p class="pt-3 text-3xl text-white">{{$objective->objective}}</p>
                         </div>
                     </div>
                 </div>
-
+            </div>
         </div>
     </section>
 </x-app-layout>
